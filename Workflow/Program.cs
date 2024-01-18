b5f2f63da9f50a5a8c64ab395b40b9d585decc58
Ray Kuo <ray20200720@gmail.com>
2024-01-18 14:36:41 +0800

    create Workflow

---
+++ Workflow/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Activities;
+using System.Activities.Statements;
+using System.Activities.XamlIntegration;
+
+namespace WorkflowExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var helloWorldActivity = new Sequence()
+            {
+                Activities =
+                {
+                    new WriteLine
+                    {
+                        Text = "Hello World!"
+                    }
+                }
+            };
+
+            System.Activities.WorkflowInvoker.Invoke(helloWorldActivity);
+        }
+    }
+}
