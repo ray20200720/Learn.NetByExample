6b895fc277bcd6a0a200a196c82701dd824d915c
Ray Kuo <ray20200720@gmail.com>
2024-01-18 17:40:01 +0800

    update Workflow: add new example from microsoft

--- Workflow/Program.cs
+++ Workflow/Program.cs
@@ -9,7 +9,8 @@ namespace WorkflowExample
     {
         static void Main(string[] args)
         {
-
+            // https://github.com/UiPath/CoreWF/blob/develop/README.md
+            
             var helloWorldActivity = new Sequence()
             {
                 Activities =
@@ -22,6 +23,53 @@ namespace WorkflowExample
             };
 
             System.Activities.WorkflowInvoker.Invoke(helloWorldActivity);
+            
+
+            // https://learn.microsoft.com/zh-cn/dotnet/framework/windows-workflow-foundation/using-workflowinvoker-and-workflowapplication
+
+            /*
+            Activity wf1 = new WriteLine
+            {
+                Text = "Hello"
+            };
+
+            WorkflowInvoker.Invoke(wf1);
+            */
+
+            /*
+            Activity wf2 = new Sequence()
+            {
+                Activities =
+                {
+                    new WriteLine()
+                    {
+                        Text = "Before the 1 minute delay."
+                    },
+                    new Delay()
+                    {
+                        Duration = TimeSpan.FromMinutes(1)
+                    },
+                    new WriteLine()
+                    {
+                        Text = "After the 1 minute delay."
+                    }
+                }
+            };
+
+            // This workflow completes successfully.
+            WorkflowInvoker.Invoke(wf2, TimeSpan.FromMinutes(2));
+
+            // This workflow does not complete and a TimeoutException
+            // is thrown.
+            try
+            {
+                WorkflowInvoker.Invoke(wf2, TimeSpan.FromSeconds(30));
+            }
+            catch (TimeoutException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            */
         }
     }
 }
